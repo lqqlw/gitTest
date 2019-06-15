0ecbd84d2ecae7c0c68f0c11a0702f587de3f8b6
zhangsan <zhangsan@itcast.cn>
2019-06-15 17:23:23 +0800

    添加了一个工程

---
+++ src/com/itheima/test.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("help me!!!!!!!!!!!");
+        System.out.println("help me!!!!!!!!!!!");
+        System.out.println("help me!!!!!!!!!!!");
+        System.out.println("help me!!!!!!!!!!!");
+        System.out.println("help me!!!!!!!!!!!");
+        System.out.println("help me!!!!!!!!!!!");
+    }
+}
